2401e713f6a6f40cd3137e5a4fba5ad861b4b202
Simon Barber <simon@pop-os.localdomain>
2021-02-17 15:06:41 -0500

    use calendar for google on this day

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -2,26 +2,47 @@ import * as React from "react";
 
 type QueryType = {
   query: {
-    selected?: string
-  }
-}
+    selected?: string;
+  };
+};
 
-export const getServerSideProps = ({ query }: QueryType) => {
-  const selectedDate = query?.selected ?? null;
+export const getServerSideProps = async ({ query }: QueryType) => {
+  const dateString: string = query?.selected ?? "";
+  const selectedDate = new Date(dateString);
+
+  let onThisDayResponse;
+  const day = selectedDate.getDay();
+  const month = selectedDate.getMonth();
+
+  await fetch(
+    `https://en.wikipedia.org/api/rest_v1/feed/onthisday/all/${month}/${day}`
+  )
+    .then((response) => response.json())
+    .then((data) => (onThisDayResponse = data));
+
+  console.log(onThisDayResponse);
 
   return {
     props: {
-      selectedDate,
+      dateString,
+      onThisDayResponse,
     },
   };
 };
 
 interface CalendarDateProps {
-    selectedDate: Date
+  dateString: string;
 }
 
 const CalendarDate = (props: CalendarDateProps) => {
-return <>Welcome to {props.selectedDate}</>;
+  return (
+    <>
+      <h1>Welcome to {props.dateString}</h1>
+      <div>
+        {props.onThisDayResponse.selected[0].text}
+      </div>
+    </>
+  );
 };
 
 export default CalendarDate;
